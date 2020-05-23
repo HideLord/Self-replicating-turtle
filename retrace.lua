b0dae464a690ef0c83679c04247345eb4d823805
HideLord <polimonom@gmail.com>
2020-05-23 19:57:00 +0300

    CC: Working chop tree basic version.

--- retrace.lua
+++ retrace.lua
@@ -4,214 +4,284 @@ movementHistory = {}
  --[=[
     Constants used to indicate movement or direction.   
  ]=]
-FORWARD = 0;
-BACKWARD = 1;
-UPWARD = 2;
-DOWNWARD = 3;
+FORWARD = 0
+BACKWARD = 1
+UPWARD = 2
+DOWNWARD = 3
 
-TURN_RIGHT = 1;
-TURN_LEFT = 3; -- 3 to avoid negative numbers
+TURN_RIGHT = 1
+TURN_LEFT = 3 -- 3 to avoid negative numbers
 
  --[=[
     required is the number of blocks the trip is going to take.
     Checks if there is enough fuel and refuels if needed.
  ]=]
 function checkForFuel(required)
-    assert(type(required) == "number", "Expected number.");
+    assert(type(required) == "number", "Expected number.")
 
     required = required or 640
     if turtle.getFuelLevel() < required then
         for i = 1, 16 do
-            local item = turtle.getItemDetail(i);
+            local item = turtle.getItemDetail(i)
 
             if item and item.name == "minecraft:coal" then
-                turtle.select(i);
+                turtle.select(i)
                 while turtle.getItemCount(i) > 0 and 
-                      turtle.getFuelLevel() < required do turtle.refuel(1); end
+                      turtle.getFuelLevel() < required do turtle.refuel(1) end
                 if turtle.getFuelLevel() >= required then
-                    return true;
+                    return true
                 end
             end
 
         end
-        return false;
+        return false
     else
-        return true;
+        return true
     end
 end
 
  -- Wrapper functions to track movement history.
 function forward(numMoves)
-    assert(type(numMoves) == "number", "Expected number.");
+    numMoves = numMoves or 1
+    assert(type(numMoves) == "number", "Expected number.")
 
-    numMoves = numMoves or 1;
     for i=1, numMoves do
-        turtle.forward(1);
-        table.insert(movementHistory,{dir = turtleInfo.dir, move = FORWARD});
+        turtle.forward(1)
+        table.insert(movementHistory,{dir = turtleInfo.dir, move = FORWARD})
     end
 end
 
 function backward(numMoves)
-    assert(type(numMoves) == "number", "Expected number.");
-
-    numMoves = numMoves or 1;
+    numMoves = numMoves or 1
+    assert(type(numMoves) == "number", "Expected number.")
+    
     for i=1, numMoves do
-        turtle.back(1);
-        table.insert(movementHistory,{dir = turtleInfo.dir, move = BACKWARD});
+        turtle.back(1)
+        table.insert(movementHistory,{dir = turtleInfo.dir, move = BACKWARD})
     end
 end
 
 function upward(numMoves)
-    assert(type(numMoves) == "number", "Expected number.");
+    numMoves = numMoves or 1
+    assert(type(numMoves) == "number", "Expected number.")
 
-    numMoves = numMoves or 1;
     for i=1, numMoves do
-        turtle.up(1);
-        table.insert(movementHistory,{dir = turtleInfo.dir, move = UPWARD});
+        turtle.up(1)
+        table.insert(movementHistory,{dir = turtleInfo.dir, move = UPWARD})
     end
 end
 
 function downward(numMoves)
-    assert(type(numMoves) == "number", "Expected number.");
+    numMoves = numMoves or 1
+    assert(type(numMoves) == "number", "Expected number.")
 
-    numMoves = numMoves or 1;
     for i=1, numMoves do
-        turtle.down(1);
-        table.insert(movementHistory,{dir = turtleInfo.dir, move = DOWNWARD});
+        turtle.down(1)
+        table.insert(movementHistory,{dir = turtleInfo.dir, move = DOWNWARD})
     end
 end
 
 function turnLeft(times)
-    assert(type(times) == "number", "Expected number.");
+    times = times or 1
+    assert(type(times) == "number", "Expected number.")
 
-    times = times or 1;
-    times = times % 4;
+    times = times % 4
     if(times > 2) then
-        turnRight(times-2);
+        turnRight(times-2)
     else
         for i=1, times do
-            turtle.turnLeft();
-            turtleInfo.dir = turtleInfo.dir + TURN_LEFT;
-            if turtleInfo.dir >= 4 then turtleInfo.dir = turtleInfo.dir - 4; end
+            turtle.turnLeft()
+            turtleInfo.dir = turtleInfo.dir + TURN_LEFT
+            if turtleInfo.dir >= 4 then turtleInfo.dir = turtleInfo.dir - 4 end
         end
     end
 end
 
 function turnRight(times)
-    assert(type(times) == "number", "Expected number.");
+    times = times or 1
+    assert(type(times) == "number", "Expected number.")
 
-    times = times or 1;
-    times = times % 4;
+    times = times % 4
     if(times > 2) then
-        turnLeft(times-2);
+        turnLeft(times-2)
     else
         for i=1, times do
-            turtle.turnRight();
-            turtleInfo.dir = turtleInfo.dir + TURN_RIGHT;
-            if turtleInfo.dir >= 4 then turtleInfo.dir = turtleInfo.dir - 4; end
+            turtle.turnRight()
+            turtleInfo.dir = turtleInfo.dir + TURN_RIGHT
+            if turtleInfo.dir >= 4 then turtleInfo.dir = turtleInfo.dir - 4 end
         end
     end
 end
 
- -- Simple tunnel 1x1. Checks for trip to and back fuel.
-function tunnelForward(blocks)
-    assert(type(blocks) == "number", "Expected number.");
+function digAndMove(movement)
+    assert(type(movement) == "number", "Expected number.")
 
-    if not checkForFuel(blocks * 2) then return false; end
-    for i = 1, blocks do
-        turtle.dig();
-        forward();
+    if movement == FORWARD then
+        turtle.dig()
+        forward()
+    elseif movement == UPWARD then
+        turtle.digUp()
+        upward()
+    else 
+        turtle.digDown()
+        downward()
     end
-    return true;
 end
 
 function matchDirection(wantedDirection)
-    assert(type(wantedDirection) == "number", "Expected number.");
+    assert(type(wantedDirection) == "number", "Expected number.")
 
-    if wantedDirection > turtleInfo.dir + 2 then
-        turtle.turnLeft();
-        turtle.turnLeft();
-    elseif wantedDirection + 2 < turtleInfo.dir then
-        turtle.turnRight();
-        turtle.turnRight();
-    elseif wantedDirection == turtleInfo.dir+1 then
-        turtle.turnRight();
-    else
-        turtle.turnLeft();
-    end
-    turtleInfo.dir = wantedDirection;
+    local dif = wantedDirection - turtleInfo.dir
+    
+    if dif < 0 then turnLeft(-dif)
+    else turnRight(dif) end
 end
 
  -- Retrace the turtle's last numMoves moves or as much as possible. 
 function retrace(numMoves)
-    assert(type(numMoves) == "number", "Expected number.");
+    numMoves = numMoves or 1
+    assert(type(numMoves) == "number", "Expected number.")
 
-    numMoves = numMoves or 1;
     if numMoves > #movementHistory then
         numMoves = #movementHistory
     end
     
     if not checkForFuel(numMoves) then
-      return false;
+      return false
     end
 
     for i=1, numMoves do
-        local currMove = movementHistory[#movementHistory]; 
+        local currMove = movementHistory[#movementHistory] 
 
         if currMove.dir ~= turtleInfo.dir then 
-            matchDirection(currMove.dir);
+            matchDirection(currMove.dir)
         end
 
         if currMove.move == FORWARD then
-            turtle.back(1);
+            turtle.back(1)
         elseif currMove.move == BACKWARD then
-            turtle.forward(1);
+            turtle.forward(1)
         elseif currMove.move == UPWARD then
-            turtle.down(1);
+            turtle.down(1)
         else
-            turtle.up(1);
+            turtle.up(1)
         end
-        table.remove(movementHistory);
+        table.remove(movementHistory)
     end
-    return true;
+    return true
 end
 
 function findItem(itemName)
-    assert(type(itemName) == "string", "Expected string.");
+    assert(type(itemName) == "string", "Expected string.")
     for i=1,16 do
-        local itemInfo = turtle.getItemDetail(i);
+        local itemInfo = turtle.getItemDetail(i)
         if itemInfo and itemInfo.name == itemName then
-            return i;
+            return i
         end
     end
-    return nil;
+    return nil
 end
 
 function findItemAndSelect(itemName)
-    local slotId = findItem(itemName);
+    local slotId = findItem(itemName)
     if(slotId) then
-        turtle.select(slotId);
-        return true;
+        turtle.select(slotId)
+        return true
     end
-    return false;
+    return false
+end
+
+--[=[Requires a log to be infront for the turtle. Uses dfs to mine the tree.
+     Returns to start. If it does not have enough fuel it will not finish the tree.
+     complete(bool) signifies whether to search surrounding blocks for logs.
+     Much more expensive and slow, but chops more complex trees]=]
+function chopTree(logType, complete)
+    assert(type(logType) == "string", "Expected string.")
+
+    local checked = {} -- The already checked blocks.
+    local startingDir = turtleInfo.dir -- Used to track the coordinates
+
+    -- Uses startingDir to calculate the coordinates after the next move. Also eww. Rework this.
+    local function getCoord(x,y,z,movement)
+        if movement == UPWARD then
+            return {x=x,y=y+1,z=z}
+        elseif movement == DOWNWARD then
+            return {x=x,y=y-1,z=z}
+        elseif movement == FORWARD then
+            if turtleInfo.dir == startingDir then
+                return {x=x+1,y=y,z=z}
+            end
+            if turtleInfo.dir == (startingDir + 1)%4 then
+                return {x=x,y=y,z=z+1}
+            end
+            if turtleInfo.dir == (startingDir + 2)%4 then
+                return {x=x-1,y=y,z=z}
+            end
+            if turtleInfo.dir == (startingDir + 3)%4 then
+                return {x=x,y=y,z=z-1}
+            end
+        elseif movement == BACKWARD then
+            if turtleInfo.dir == startingDir then
+                return {x=x-1,y=y,z=z}
+            end
+            if turtleInfo.dir == (startingDir + 1)%4 then
+                return {x=x,y=y,z=z-1}
+            end
+            if turtleInfo.dir == (startingDir + 2)%4 then
+                return {x=x+1,y=y,z=z}
+            end
+            if turtleInfo.dir == (startingDir + 3)%4 then
+                return {x=x,y=y,z=z+1}
+            end
+        else
+            assert(false==true, "chopTree::getCoord: Unknown direction")
+        end
+    end
+
+    local function dfs(depth, x, y, z)
+        checked[{x,y,z}] = true
+        
+        local function dfsHelper(movement, hasBlockInfo, info)
+            if hasBlockInfo and string.find(info.name,logType) then
+                digAndMove(movement)
+                newCoord = getCoord(x,y,z,movement)
+                dfs(depth+1,newCoord.x,newCoord.y,newCoord.z)
+            end
+        end
+        
+        dfsHelper(FORWARD,turtle.inspect())
+        for i=1, 3 do
+            turnLeft()
+            dfsHelper(FORWARD,turtle.inspect())
+        end
+        dfsHelper(UPWARD,turtle.inspectUp())
+        dfsHelper(DOWNWARD,turtle.inspectDown())
+
+        retrace()
+    end
+
+    dfs(0,0,0,0)
+    matchDirection(startingDir)
 end
 
 function treeFarm()
-    function findSaplingAndSelect()
-        if(findItemAndSelect("minecraft:oakSapling")) then return true end
+    local function findSaplingAndSelect()
+        if(findItemAndSelect("minecraft:sapling")) then return true end
         if(findItemAndSelect("minecraft:birchSapling")) then return true end
         if(findItemAndSelect("minecraft:spruceSapling")) then return true end
-        return false;
+        return false
     end
-    function plantSapling()
+    local function plantSapling()
         if not findSaplingAndSelect() then return false end
         turtle.placeDown()
     end
-    function waitForLog()
+    local function waitForLog()
         while true do
             local blockInfo = turtle.inspect()
-            if string.find(blockInfo.name,"log") then break end
+            if string.find(blockInfo.name,"log") or string.find(blockInfo.name,"Log") then break end
             os.sleep(1)
         end
     end
 end
+
+chopTree("log", false)
